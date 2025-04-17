5178557be00f34e0f60b2b6d8f8176750cab8a43
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-04-17 11:38:44 +0300

    Add a mistake by using empty block

--- src/test/java/eu/senla/AppTest.java
+++ src/test/java/eu/senla/AppTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import org.w3c.dom.xpath.*;
+
 public class AppTest {
 
     @Test
@@ -11,5 +13,8 @@ public class AppTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.close();
+    }
+    {
+
     }
 }
