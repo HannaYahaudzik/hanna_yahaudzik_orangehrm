d1b5fe2c258e75ae9f7bfe13fc07a5e2595c5362
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-05-21 15:48:06 +0300

    Edit a test for required field.

--- src/main/java/eu/senla/entity/User.java
+++ src/main/java/eu/senla/entity/User.java
@@ -1,39 +1,43 @@
 package eu.senla.entity;
 
+import lombok.Builder;
+import lombok.Getter;
 
+@Builder
+@Getter
 public class User {
-    private final String username;
-    private final String password;
+    private String username;
+    private String password;
 
-    public User(UserBuilder builder) {
-        this.username = builder.username;
-        this.password = builder.password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public static class UserBuilder {
-        public String username;
-        public String password;
-
-        public UserBuilder username(final String username) {
-            this.username = username;
-            return this;
-        }
-
-        public UserBuilder password(final String password) {
-            this.password = password;
-            return this;
-        }
-
-        public User build() {
-            return new User(this);
-        }
-    }
+//    public User(UserBuilder builder) {
+//        this.username = builder.username;
+//        this.password = builder.password;
+//    }
+//
+//    public String getUsername() {
+//        return username;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public static class UserBuilder {
+//        public String username;
+//        public String password;
+//
+//        public UserBuilder username(final String username) {
+//            this.username = username;
+//            return this;
+//        }
+//
+//        public UserBuilder password(final String password) {
+//            this.password = password;
+//            return this;
+//        }
+//
+//        public User build() {
+//            return new User(this);
+//        }
+//    }
 }
