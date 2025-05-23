4723dceffca1f66bb2d9544f490fc725a01b9e4d
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-05-23 09:38:47 +0300

    Add a display name for tests

--- src/test/java/eu/senla/LoginTest.java
+++ src/test/java/eu/senla/LoginTest.java
@@ -5,25 +5,29 @@ import eu.senla.entity.User;
 import eu.senla.general.BaseTest;
 import eu.senla.pageObject.login.LoginPage;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
 
-
+@DisplayName("Проверка формы Логин")
 public class LoginTest extends BaseTest {
 
     @Test
+    @DisplayName("Проверка успешного логина с валидными данными")
     public void loginSuccess() {
         new LoginPage().loginValidUser().getPageHeader();
         Assertions.assertTrue(Driver.getInstance().getCurrentUrl().contains("/dashboard/index"));
     }
 
     @Test
+    @DisplayName("Проверка ошибки при входя с невалидными данными")
     public void loginInvalidUser() {
         Assertions.assertEquals("Invalid credentials", new LoginPage().loginFakerUser().getErrorMessage());
     }
@@ -33,17 +37,21 @@ public class LoginTest extends BaseTest {
      * @param user
      * @param expectedRequiredFieldName
      */
-    @ParameterizedTest
+    @ParameterizedTest(name = "{index} - Проверка обязательных полей: {1}")
     @MethodSource("requiredFieldData")
+    @DisplayName("Проверка заполнения обязательных полей")
     public void testRequiredField(final User user, final List<String> expectedRequiredFieldName) {
         Assertions.assertEquals(expectedRequiredFieldName, new LoginPage().loginUser(user).getRequiredFieldNameWithError());
     }
 
     static Stream<Arguments> requiredFieldData() {
         return Stream.of(
-                Arguments.arguments(new User.UserBuilder().username("").password("").build(), Arrays.asList("username", "password")),
-                Arguments.arguments(new User.UserBuilder().username("username").password("").build(), Arrays.asList("password")),
-                Arguments.arguments(new User.UserBuilder().username("").password("password").build(), Arrays.asList("username"))
+                Arguments.arguments(
+                        new User.UserBuilder().username("").password("").build(), Arrays.asList("username", "password")),
+                Arguments.arguments(
+                        new User.UserBuilder().username("username").password("").build(), Collections.singletonList("password")),
+                Arguments.arguments(
+                        new User.UserBuilder().username("").password("password").build(), Collections.singletonList("username"))
         );
     }
 }
--- src/test/java/eu/senla/adminPage/addEmployee/AddEmployeeTest.java
+++ src/test/java/eu/senla/adminPage/addEmployee/AddEmployeeTest.java
@@ -2,11 +2,14 @@ package eu.senla.adminPage.addEmployee;
 
 import eu.senla.general.BaseTest;
 import eu.senla.pageObject.login.LoginPage;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+@DisplayName("Проверка формы добавления сотрудника")
 public class AddEmployeeTest extends BaseTest {
 
     @Test
+    @DisplayName("Проверка заполнения формы добавления сотрудника")
     public void addEmployee() {
         new LoginPage()
                 .loginValidUser()
